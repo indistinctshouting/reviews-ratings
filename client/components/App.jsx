03ecada47e9dc8a86d2656eafc995f29e8058fa8
Javi Garcia-Bengochea <jgb4410@gmail.com>
2018-09-07 18:56:42 -0700

    Added get data method

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,13 +4,19 @@ import axios from 'axios';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    axios.post('/reviews', {id: 2})
-      .then((response) => {
-        console.log(response.data);
-      });
+    this.state = {
+      reviews = []
+    }
   }
 
-
+  getReviewData(id) {
+    axios.post('/reviews', {id})
+      .then((response) => {
+        this.setState({
+          reviews: response.data
+        })
+      });
+  }
 
   render() {
     return (
