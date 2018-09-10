eedf88448af1dae0a90662800147b2286a2bc6e7
Javi Garcia-Bengochea <jgb4410@gmail.com>
2018-09-10 15:43:15 -0700

    Created and exported barebones class component for Reviews

---
+++ client/components/Review.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Review extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // state vars tbd but I think I'll need em
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Yeah!
+      </div>
+    );
+  }
+}
